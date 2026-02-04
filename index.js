4920ee6a9ba47ba8885099fc155daced927d343b
Shuvo <thedragonboyz79@gmail.com>
2026-02-05 01:01:00 +0600

    third commit

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["chrome-extension://ajgljojgkknncdbcihhghdlaeifbdhba"]
+}));
+
 
 connectDB()
 .then(() => console.log('MongoDB connected'))
